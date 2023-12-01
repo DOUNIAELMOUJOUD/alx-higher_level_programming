6352fd30333c0a18347bdcd29b7c61c58a02cc5b
DOUNIAELMOUJOUD <anouarmoujoud10@gmail.com>
2023-12-01 04:50:39 -0800

    3-say_my_name.py

---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python3
+
+"""Defines a square-printing function."""
+
+
+def print_square(size):
+    """Print a square with the # character.
+    Args:
+        size (int): The height/width of the square.
+    Raises:
+        TypeError: If size is not an integer.
+        ValueError: If size is < 0
+    """
+    if not isinstance(size, int):
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+
+    for i in range(size):
+        [print("#", end="") for j in range(size)]
+        print("")
+
+0x07-python-test_driven_development/5-text_indentation.py
+
+#!/usr/bin/python3
+
+"""Defines a text-indentation function."""
+
+
+def text_indentation(text):
+    """Print text with two new lines after each '.', '?', and ':'.
+    Args:
+        text (string): The text to print.
+    Raises:
+        TypeError: If text is not a string.
+    """
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+
+    c = 0
+    while c < len(text) and text[c] == ' ':
+        c += 1
+
+    while c < len(text):
+        print(text[c], end="")
+        if text[c] == "\n" or text[c] in ".?:":
+            if text[c] in ".?:":
+                print("\n")
+            c += 1
+            while c < len(text) and text[c] == ' ':
+                c += 1
+            continue
+        c += 1
+
