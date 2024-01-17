1ef1ebbb19863861fa5abeead93ffef9f3e54e27
DOUNIAELMOUJOUD <anouarmoujoud10@gmail.com>
2024-01-17 08:35:40 -0800

    0x0E-SQL_more_queries

--- 0x0E-SQL_more_queries/102-rating_shows.sql
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -1,6 +1,7 @@
 -- lists all shows from hbtn_0d_tvshows_rate by their rating
-SELECT title, SUM(tv_show_ratings.rate) ‘rating’
-FROM tv_shows
+
+
+SELECT title, SUM(tv_show_ratings.rate) ‘rating’ FROM tv_shows
 LEFT JOIN tv_show_ratings ON tv_show_ratings.shows_id = tv_shows.id
 GROUP BY title
 ORDER BY rating DESC;
