213d4d88e6e1cd639f0332323ec7a12ad90a9cca
DOUNIAELMOUJOUD <anouarmoujoud10@gmail.com>
2024-03-01 14:41:40 +0100

    0-hbtn_status.py

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,14 +1,13 @@
 #!/usr/bin/python3
-"""script for testing status of web pages
-"""
+"""Script that fetches https://alx-intranet.hbtn.io/status."""
+from urllib.request import Request, urlopen
+
+
 if __name__ == "__main__":
-        import urllib.request
-            url = "https://intranet.hbtn.io/status"
-                with urllib.request.urlopen(url) as response:
-                            bytes_content = response.read()
-                                    content = bytes_content.decode('utf-8')
-                                            print_str = '''Body response:
-                                            \t- type: {}
-                                            \t- content: {}
-                                            \t- utf8 content: {}'''.format(type(bytes_content), bytes_content, content)
-                                                    print(print_str)
+        req = Request("https://alx-intranet.hbtn.io/status")
+            with urlopen(req) as response:
+                        body = response.read()
+                                print("Body response:")
+                                        print("\t- type: {}".format(type(body)))
+                                                print("\t- content: {}".format(body))
+                                                        print("\t- utf8 content: {}".format(body.decode("utf-8")))
